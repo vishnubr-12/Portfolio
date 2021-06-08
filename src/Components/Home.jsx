7d10d23c57fddb29f334c8f396dca5fa1cd501ad
vishnubr-12 <divyarajanikanth.m@gmail.com>
2021-06-08 20:04:50 +0530

    bug fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Grid,Hidden} from '@material-ui/core';
+import {Grid,Hidden,makeStyles,} from '@material-ui/core';
 import {Text,Image} from "./Homecontent";
-
+const useStyles = makeStyles((theme) => ({img:{paddingRight: '25px',[theme.breakpoints.down('xs')]:{paddingRight: '5px'}}}));
 const Home = () => {
+    const classes = useStyles();
     return (
         <>
         <Grid container spacing={3}>
         <Hidden xsDown >
             <Text/>  
         </Hidden>
-        <Grid item md={6} sm={6} xs={12} style={{paddingRight: '25px'}}>
+        <Grid item md={6} sm={6} xs={12} className={classes.img}>
             <Image/>
         </Grid>
         </Grid>
